88771eba4b36d3c3f9b58e2420a0d25c0f2b2300
Mark Horton <mark@nostromo.net>
2015-05-08 19:44:32 -0400

    initial

---
+++ src/main/java/net/nostromo/utils/Utils.java
@@ -0,0 +1,30 @@
+package net.nostromo.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public class Utils {
+
+    public static final Charset UTF8 = Charset.forName("utf-8");
+    public static final char NL = '\n';
+
+    public static String readClassPathFile(final String fileName) throws IOException {
+        final ClassLoader cl = Utils.class.getClassLoader();
+
+        try (final InputStream is = cl.getResourceAsStream(fileName);
+             final BufferedReader br = new BufferedReader(new InputStreamReader(is, UTF8))) {
+            final StringBuilder sb = new StringBuilder(1024);
+
+            while (true) {
+                final String line = br.readLine();
+                if (line == null) break;
+                sb.append(line).append(NL);
+            }
+
+            return sb.toString();
+        }
+    }
+}
